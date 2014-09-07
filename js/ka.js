1524f146765ea3aaf6351ead86b3095b9afdee69
Brian R. Bondy <netzen@gmail.com>
2014-09-07 10:10:19 -0400

    Add Backbone React mixin for updating UI when properties change.
    This is only currently needed because of changing the topic model
    But may also be needed for things like showing / storing points on video
    models and perhaps storing completion status on models as well.
    
    Test Plan:
    - Download a video, go to drawer without navigating back to topic list.
      Should show immediately the change.

--- js/ka.js
+++ js/ka.js
@@ -55,6 +55,31 @@ define(["oauth", "storage"], function(_oauth, Storage) {
           return function() {
               return fn.apply(this, args.concat(aps.call(arguments)));
           }
+        },
+        // An example generic Mixin that you can add to any component that should react
+        // to changes in a Backbone component. The use cases we've identified thus far
+        // are for Collections -- since they trigger a change event whenever any of
+        // their constituent items are changed there's no need to reconcile for regular
+        // models. One caveat: this relies on getBackboneModels() to always return the
+        // same model instances throughout the lifecycle of the component. If you're
+        // using this mixin correctly (it should be near the top of your component
+        // hierarchy) this should not be an issue.
+        // https://github.com/facebook/react/blob/1be9a9e/examples/todomvc-backbone/js/app.js#L148-L171
+        BackboneMixin : {
+          componentDidMount: function() {
+            // Whenever there may be a change in the Backbone data, trigger a reconcile.
+            this.getBackboneModels().forEach(function(model) {
+              model.on('add change remove', this.forceUpdate.bind(this, null), this);
+            }, this);
+          },
+
+          componentWillUnmount: function() {
+            // Ensure that we clean up any dangling references when the component is
+            // destroyed.
+            this.getBackboneModels().forEach(function(model) {
+              model.off(null, null, this);
+            }, this);
+          }
         }
     };
 
