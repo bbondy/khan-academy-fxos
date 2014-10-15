99dee690159190e6fe4343f51d1cb1b45b4e8b18
Brian R. Bondy <netzen@gmail.com>
2014-10-14 23:18:16 -0400

    Add some more tests for models related init
    Test Plan:
    - Run tests

--- test/test.js
+++ test/test.js
@@ -9,4 +9,18 @@ require(["react", "models", "apiclient"], function(React, models, APIClient) {
             QUnit.start();
         });
     });
+    QUnit.asyncTest( "models.TopicTree.init", function(assert) {
+        expect(1);
+        models.TopicTree.init().done(function() {
+            assert.ok(models.TopicTree.root);
+            QUnit.start();
+        });
+    });
+    QUnit.asyncTest( "models.CurrentUser.init", function(assert) {
+        expect(1);
+        models.CurrentUser.init().done(function() {
+            assert.ok(models.CurrentUser.initialized);
+            QUnit.start();
+        });
+    });
 });
