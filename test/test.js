768482cd2bd2bc1f1eb1622b0ef0eecf0d63eeaf
Brian R. Bondy <netzen@gmail.com>
2014-10-18 10:38:07 -0400

    Add some more tests
    Test Plan:
    - Run tests with ./tools/runtests

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
-require(["react", "models", "apiclient"], function(React, models, APIClient) {
+require(["react", "models", "apiclient", "storage", "downloads", "cache"], function(React, models, APIClient, Storage, Downloads, Cache) {
 
-    QUnit.asyncTest( "APIClient.init", function(assert) {
+    QUnit.asyncTest("APIClient.init", function(assert) {
         expect(2);
         APIClient.init().done(function() {
             // Consumer key and secret should be available after init
@@ -9,18 +9,52 @@ require(["react", "models", "apiclient"], function(React, models, APIClient) {
             QUnit.start();
         });
     });
-    QUnit.asyncTest( "models.TopicTree.init", function(assert) {
-        expect(1);
+    QUnit.asyncTest("models.TopicTree.init", function(assert) {
+        expect(4);
         models.TopicTree.init().done(function() {
             assert.ok(models.TopicTree.root);
+            assert.ok(models.TopicTree.allContentItems.length > 0);
+            // Topics are at root
+            assert.ok(models.TopicTree.root.get("topics").models.length > 0);
+            // But there are no content items at root
+            assert.ok(models.TopicTree.root.get("contentItems").models.length === 0);
             QUnit.start();
         });
     });
-    QUnit.asyncTest( "models.CurrentUser.init", function(assert) {
+    QUnit.asyncTest("models.CurrentUser.init", function(assert) {
         expect(1);
         models.CurrentUser.init().done(function() {
             assert.ok(models.CurrentUser.initialized);
             QUnit.start();
         });
     });
+    QUnit.asyncTest("models.AppOptions.fetch", function(assert) {
+        expect(1);
+        models.AppOptions.fetch().done(function() {
+            assert.ok(models.AppOptions.get("showDownloadsOnly") === false);
+            QUnit.start();
+        });
+    });
+    QUnit.asyncTest("Storage.init", function(assert) {
+        expect(1);
+        Storage.init().done(function() {
+            assert.ok(true);
+            QUnit.start();
+        });
+    });
+    QUnit.asyncTest("Downloads.init", function(assert) {
+        expect(1);
+        Downloads.init().done(function() {
+            assert.ok(true);
+            QUnit.start();
+        });
+    });
+    QUnit.asyncTest("Cache.init", function(assert) {
+        expect(1);
+        Cache.init().done(function() {
+            assert.ok(true);
+            QUnit.start();
+        });
+    });
+
 });
