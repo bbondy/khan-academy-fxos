ea7187ce86445ec4819a92b1a1354921cf4f0c63
Brian R. Bondy <brian@khanacademy.org>
2014-12-12 21:30:10 -0500

    Remove youtube player and fix videojs player + react problems
    React doesn't work well when you modify the dom with sibling elements.
    Using child elements works fine though. So this allows videojs + react
    to work well together.
    
    Test Plan:
    - Play some videos

--- test/test-react.js
+++ test/test-react.js
@@ -85,7 +85,6 @@ require(["react-dev", "util", "models", "apiclient", "storage", "downloads", "ca
             assert.ok(_(videoItems).some(function(videoItem) {
                 return videoItem.props.video.getTitle() === "Harlow monkey experiments";
             }));
-            models.AppOptions.set("useYouTubePlayer", false);
 
             // Test that video view render with transcripts
             models.AppOptions.set("showTranscripts", true);
