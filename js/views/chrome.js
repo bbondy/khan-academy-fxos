a73f786def4d01deb4e34f772a5c36697a50319d
Brian R. Bondy <brian@khanacademy.org>
2014-12-19 19:27:54 -0500

    Fix failing react test due to duplicate component

--- js/views/chrome.js
+++ js/views/chrome.js
@@ -113,67 +113,6 @@ define([window.isTest ? "react-dev" : "react", "util", "models", "apiclient", "c
         }
     });
 
-
-    /**
-     * Represents the topic search input item which is right below the header.
-     */
-    var TopicSearch = React.createClass({
-        propTypes: {
-            model: React.PropTypes.object.isRequired,
-            onTopicSearch: React.PropTypes.func.isRequired
-        },
-        getInitialState: function() {
-            return {value: ''};
-        },
-        componentWillReceiveProps: function() {
-            this.state.value = '';
-        },
-        onChange: function(event) {
-            var topicSearch = event.target.value;
-            this.setState({value: topicSearch});
-            this.props.onTopicSearch(topicSearch);
-        },
-        handleFocus: function(event) {
-            setTimeout(() => {
-                $("html, body").stop(true, true).animate({
-                    scrollTop: $(this.refs.search.getDOMNode()).offset().top
-                }, 500);
-            }, 500);
-        },
-        handleBlur: function(event) {
-            $("html, body").stop(true, true).animate({
-                scrollTop: 0
-            }, 700);
-        },
-
-        render: function() {
-            var style = {
-                width: "100%",
-                height: "3em;",
-                position: "relative"
-            };
-            var text = document.webL10n.get("search");
-            if (this.props.model.getTitle()) {
-                text = document.webL10n.get("search-topic",
-                        {"topic": this.props.model.getTitle()});
-            }
-            return <div>
-                <input ref="search"
-                       className="search"
-                       type="searh"
-                       placeholder={text}
-                       value={this.state.value}
-                       required=""
-                       style={style}
-                       onChange={this.onChange}
-                       onFocus={this.handleFocus}
-                       onBlur={this.handleBlur}
-                       />
-            </div>;
-
-        }
-    });
-
     var StatusBarViewer = React.createClass({
         render: function() {
             if (!models.TempAppState.get("status")) {
