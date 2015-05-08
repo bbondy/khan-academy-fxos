a356431bd3d6e480fa839d54e8d8491df95b5b11
Brian R. Bondy <netzen@gmail.com>
2015-05-07 22:33:28 -0400

    Split out transcript actions

--- src/views/video-actions.js
+++ src/views/video-actions.js
@@ -1,28 +1,5 @@
-import {isVideo, getId, getYoutubeId, isDownloaded} from "../data/topic-tree-helper";
+import {isVideo, getId, isDownloaded} from "../data/topic-tree-helper";
 import Immutable from "immutable";
-import APIClient from "../apiclient";
-
-export const loadTranscriptIfVideo = (options, editVideo) => (topicTreeNode) => {
-    if (!isVideo(topicTreeNode)) {
-        return topicTreeNode;
-    }
-
-    if (!options.get("showTranscripts")) {
-        return topicTreeNode;
-    }
-
-    APIClient.getVideoTranscript(getYoutubeId(topicTreeNode)).then((transcript) => {
-        if (transcript && transcript.length === 0) {
-            return;
-        }
-        editVideo((video) => {
-            return Immutable.fromJS({
-                transcript,
-            });
-        });
-    });
-    return topicTreeNode;
-};
 
 export const loadVideoIfDownloadedVideo = (editVideo) => (topicTreeNode) => {
     if (!isVideo(topicTreeNode)) {
