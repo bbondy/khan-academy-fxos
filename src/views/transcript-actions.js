a178061d4bbcf108af9f7028c71db76b16962539
Brian R. Bondy <netzen@gmail.com>
2015-05-07 22:57:22 -0400

    Don't temporarily show old transcript for new video

---
+++ src/views/transcript-actions.js
@@ -0,0 +1,29 @@
+import {isVideo, getYoutubeId} from "../data/topic-tree-helper";
+import Immutable from "immutable";
+import APIClient from "../apiclient";
+
+export const loadTranscriptIfVideo = (options, editVideo) => (topicTreeNode) => {
+    if (!isVideo(topicTreeNode) || !options.get("showTranscripts")) {
+        return topicTreeNode;
+    }
+
+    editVideo((video) =>
+        Immutable.fromJS({
+            transcript: null,
+        }))
+
+    APIClient.getVideoTranscript(getYoutubeId(topicTreeNode)).then((transcript) => {
+        if (transcript && transcript.length === 0) {
+            return editVideo((video) =>
+                Immutable.fromJS({
+                    transcript: null,
+                }));
+        }
+        editVideo((video) =>
+            Immutable.fromJS({
+                transcript,
+            }));
+    });
+    return topicTreeNode;
+};
+
